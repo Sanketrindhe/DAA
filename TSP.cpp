b716f7530fc1a4650cf3d0ad6514a9602093507f
Sanketrindhe <142704837+Sanketrindhe@users.noreply.github.com>
2024-11-13 23:54:00 +0530

    Update TSP.cpp

--- TSP.cpp
+++ TSP.cpp
@@ -1,50 +1,114 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
-#define INF 999
+#define INF INT_MAX
 
-int n, cost = 0;
+int n, finalCost = INF;
 vector<vector<int>> costMatrix;
 vector<bool> visited;
 
-void TSP(int city) {
-    visited[city] = true;
-    cout << city + 1 << " --> ";
+class Node {
+public:
+    int level, pathCost, bound;
+    vector<int> path;
 
-    int nextCity = INF, minCost = INF;
-    for (int i = 0; i < n; ++i)
-        if (!visited[i] && costMatrix[city][i] && costMatrix[city][i] < minCost)
-            minCost = costMatrix[city][i], nextCity = i;
+    Node(int l, int c, int b) {
+        level = l;
+        pathCost = c;
+        bound = b;
+    }
+};
 
-    if (nextCity == INF) {
-        cout << "1";
-        cost += costMatrix[city][0];
-    } else {
-        cost += minCost;
-        TSP(nextCity);
+int calculateBound(Node &node) {
+    int bound = node.pathCost;
+    int visitedCount = node.level;
+
+    // Find the minimum cost of reaching the next unvisited cities
+    for (int i = 0; i < n; i++) {
+        if (visited[i] == false) {
+            int minEdge = INF;
+            for (int j = 0; j < n; j++) {
+                if (i != j && visited[j] == false) {
+                    minEdge = min(minEdge, costMatrix[i][j]);
+                }
+            }
+            bound += minEdge;
+        }
+    }
+
+    return bound;
+}
+
+void TSPBranchAndBound() {
+    // Initialize the root node
+    Node root(0, 0, 0);
+    root.path.push_back(0);
+
+    vector<bool> visitedCities(n, false);
+    visitedCities[0] = true;
+
+    // Create a priority queue (min-heap)
+    vector<Node> pq;
+    pq.push_back(root);
+
+    while (!pq.empty()) {
+        Node node = pq.back();
+        pq.pop_back();
+
+        // If all cities are visited, update the final cost
+        if (node.level == n - 1) {
+            if (costMatrix[node.path[node.level]][0] != INF) {
+                int finalCostCandidate = node.pathCost + costMatrix[node.path[node.level]][0];
+                if (finalCostCandidate < finalCost) {
+                    finalCost = finalCostCandidate;
+                    for (int i = 0; i <= node.level; i++) {
+                        cout << node.path[i] + 1 << " --> ";
+                    }
+                    cout << node.path[0] + 1 << endl;
+                }
+            }
+            continue;
+        }
+
+        // Generate the child nodes
+        for (int i = 0; i < n; i++) {
+            if (!visitedCities[i] && costMatrix[node.path[node.level]][i] != INF) {
+                Node child(node.level + 1, node.pathCost + costMatrix[node.path[node.level]][i], 0);
+                child.path = node.path;
+                child.path.push_back(i);
+
+                visitedCities[i] = true;
+                child.bound = calculateBound(child);
+
+                // If the bound is less than the final cost, explore this child
+                if (child.bound < finalCost) {
+                    pq.push_back(child);
+                }
+                visitedCities[i] = false;
+            }
+        }
     }
 }
 
 int main() {
-    cout << "Enter number of cities: ";
-	cin >> n;
+    cout << "Enter the number of cities: ";
+    cin >> n;
 
-	// Resize the costMatrix and visited vectors
-	costMatrix.resize(n, vector<int>(n));
-	visited.resize(n, false);
+    costMatrix.resize(n, vector<int>(n));
 
-	cout << "Enter Cost Matrix (0 for Infinity):\n";
-	// Use traditional for loops to read the cost matrix
-	for (int i = 0; i < n; ++i) {
-    	for (int j = 0; j < n; ++j) {
-        	cin >> costMatrix[i][j];
-    		}
-	}
+    cout << "Enter the cost matrix (use " << INF << " for infinity):\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> costMatrix[i][j];
+        }
+    }
 
-    cout << "\nThe Best Path is:\n";
-    TSP(0);
-    cout << "\nMinimum Cost: " << cost << endl;
+    cout << "\nThe best path is:\n";
+    TSPBranchAndBound();
+    cout << "\nMinimum cost: " << finalCost << endl;
+
     return 0;
 }
